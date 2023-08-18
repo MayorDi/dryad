c11e06871cac57dc46e191fcfcb8295d7ff18f4b
Dmitriy Mayorov <MayorDi@yandex.ru>
2023-08-18 17:29:25 +0300

    `if let` to `match`

--- src/app/update.rs
+++ src/app/update.rs
@@ -8,14 +8,20 @@ use super::App;
 impl App {
     pub fn update(&mut self, world_read: &World) {
         for i in 0..self.world.segments.len() {
-            if let Cell(mut cell) = self.world.segments[i] {
-                cell.update(world_read, &mut self.world);
+            match self.world.segments[i] {
+                Cell(mut cell) => {
+                    cell.update(world_read, &mut self.world);
 
-                self.world.segments[i] = Cell(cell);
-            } else if let Dirt(mut block) = self.world.segments[i] {
-                block.update(world_read, &mut self.world);
+                    self.world.segments[i] = Cell(cell);
+                }
 
-                self.world.segments[i] = Dirt(block);
+                Dirt(mut block) => {
+                    block.update(world_read, &mut self.world);
+
+                    self.world.segments[i] = Dirt(block);
+                }
+
+                Air(_) => {}
             }
         }
     }
